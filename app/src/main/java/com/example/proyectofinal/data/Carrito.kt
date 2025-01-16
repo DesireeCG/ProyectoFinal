2e630b8547c3b60354001f4a3e94a0b4c45767e6
AxelOjedaHernandez <axelojedahernandez64@gmail.com>
2025-01-16 10:57:37 -0600

    regreso a inicio

--- app/src/main/java/com/example/proyectofinal/data/Carrito.kt
+++
@@ -1,7 +0,0 @@
-package com.example.proyectofinal.data
-
-data class Carrito(
-    val carritoId: Int, // ID único del carrito
-    val productoId: Int, // ID del producto agregado al carrito
-    val usuarioId: Int   // ID del usuario al que pertenece el carrito
-)
--- app/src/main/java/com/example/proyectofinal/data/Usuario.kt
+++
@@ -1,8 +0,0 @@
-package com.example.proyectofinal.data
-
-data class Usuario(
-    val id: String?=null,
-    val clave: String?=null,
-    val nombre: String?=null,
-    val correo: String?=null,
-)
